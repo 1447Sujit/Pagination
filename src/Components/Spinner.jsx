ac3502215dec9eaff59d71dc7d02c22fbc2d6f9d
1447Sujit <jsujitkumar4@gmail.com>
2024-09-04 15:09:42 +0530

    File Upload

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className='spinnerContainer'>
+      <div class="spinner"></div>
+      <h4>Loading...</h4>
+    </div>
+  )
+}
+
+export default Spinner
+
